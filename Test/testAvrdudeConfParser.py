03cd2d1423f4b5257d4ad4d34a44ead0db4e26eb
mickey9910326 <mickey9910326@gmail.com>
2018-02-24 19:57:54 +0800

    將測試檔案搬移到Test資料夾下

---
+++ Test/testAvrdudeConfParser.py
@@ -0,0 +1,14 @@
+from avrdudeConfParser import AvrdudeConfParser
+
+if __name__ == '__main__':
+    parser = AvrdudeConfParser()
+
+    s1,s2,s3 = parser.GetBasicInfoByDesc('ATmega128')
+
+    print(s1)
+    print(s2)
+    print(s3)
+
+
+    descList = parser.listAllPartDesc()
+    print(descList)
