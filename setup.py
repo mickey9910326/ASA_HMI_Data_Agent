ce25e8f956a2eb3bb119fe6959cb7d9e55b1ca7b
mickey9910326 <mickey9910326@gmail.com>
2018-04-26 07:50:24 +0800

    更新setup方法，並更新版本號為v0.2.6

--- setup.py
+++ setup.py
@@ -4,21 +4,25 @@ from PyInstaller.__main__ import run
 import pathlib
 
 if __name__ == '__main__':
-    patch = 'v0.2.5'
+    patch = 'v0.2.6'
 
-    os.system('rm -rf ./dist/ASA_HMI_Data_Agent')
+    os.system('rm -r ./dist/ASA_HMI_Data_Agent')
     os.system('pyuic5 ui/mainwindow.ui -o ui_mainwindow.py')
     os.system('pyuic5 ui/hmi.ui -o ui_hmi.py')
     os.system('pyuic5 ui/avrdude.ui -o ui_avrdude.py')
     os.system('pyuic5 ui/asa_prog.ui -o ui_asa_prog.py')
     os.system('pyuic5 ui/bit_selector.ui -o ui_bit_selector.py')
 
-    opts = ['-w',
+    opts = [
             # NOTE '-F' maybe not stable in some environment, need more test.
             '-F',
 
-            # NOTE if ypu want to print debug info in CLI, use the '-debug'.
+            # NOTE if you want exe not to print info to I/O, use '-w' .
+            '-w',
+
+            # NOTE if ypu want to extra debug info, use the '-debug'.
             # '--debug',
+            # '--windowed',
 
             # NOTE you need to change the libs path.
             # If it has been include in yout system PATH, you don't have to add it.
@@ -30,6 +34,14 @@ if __name__ == '__main__':
             '--paths=C:\\Program Files (x86)\\Windows Kits\\10\\Redist\\ucrt\\DLLs\\x86',
             '--paths=C:\\Program Files (x86)\\Windows Kits\\10\\Redist\\ucrt\\DLLs\\x64',
 
+            # NOTE Need DLLs in package numpy and scipy.
+            '--paths=D:\\Compiler\\Python36-32\\Lib\\site-packages\\scipy\\extra-dll',
+            '--paths=D:\\Compiler\\Python36-32\\Lib\\site-packages\\numpy\\.libs',
+
+            # NOTE Need extra moudle in package scipy.
+            '--hidden-import=scipy._lib.messagestream',
+
+            # '--name=ASA_HMI_Data_Agent'
             # '--icon', 'rxx.ico',
             '--noupx',
             '--clean',
