18dd5d7ee0f55491f10ac8cffb8fa8f57b754fef
mickey9910326 <mickey9910326@gmail.com>
2018-05-16 18:14:21 +0800

    完成ASA_M128燒錄功能
    - 更新requirements: 增加py_asa_loader
    - 更新asaprog\asaprog.py

--- asaprog/asaprog.py
+++ asaprog/asaprog.py
@@ -1,43 +1,65 @@
 from PyQt5.QtCore import pyqtSlot, QThread, pyqtSignal
 from PyQt5.QtWidgets import QFileDialog
 from listport import serial_ports
-import subprocess
-import time
+from time import sleep
+import py_asa_loader
+import math
 
 # ---- class ShellThread Start -------------------------------------------------
 class ShellThread(QThread):
-    signalGetLine = pyqtSignal(str)
+    signalCheckIsAsaDevice = pyqtSignal(bool)
+    signalLastData = pyqtSignal(bool)
+    signalInitProgressbar = pyqtSignal(int,int)
+    signalSetProgressbar = pyqtSignal(int)
+    signalGetSerialException = pyqtSignal()
 
     def __init__(self):
         QThread.__init__(self)
         self.cmd = str()
 
-    def setCmd(self, cmd):
-        self.cmd = cmd
+    def setParameter(self, port, hexfile):
+        self.port = port
+        self.hexfile = hexfile
 
     def run(self):
-        times = time.strftime("%H:%M:%S", time.gmtime())
-        self.signalGetLine.emit('[' + times + '] ' + self.cmd + '\n')
-        self.shellIsRunning = True
+        try:
+            loader = py_asa_loader.Loader(self.port, self.hexfile)
 
-        self.p = subprocess.Popen(self.cmd.split(' ') , stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
-        while self.p.poll() is None:
-            s = self.p.stderr.readline()
-            print(s)
-            if(s.decode("utf-8", "replace") is not ''):
-                self.signalGetLine.emit(s.decode("utf-8", "replace"))
-        times = time.strftime("%H:%M:%S", time.gmtime())
-        self.signalGetLine.emit('\n' + '[' + times + '] ' + 'Complete!' + '\n')
-        self.shellIsRunning = False
+            isAsaDevice = loader.checkIsAsaDevice()
+            self.signalCheckIsAsaDevice.emit(isAsaDevice)
+
+            if isAsaDevice is False:
+                return
+
+            times = math.floor(len(loader.bin)/256)
+            remain = len(loader.bin)%256
+
+            if remain is not 0:
+                self.signalInitProgressbar.emit(0,times+1)
+            else:
+                self.signalInitProgressbar.emit(0,times)
+
+            delay = 0.03
+            for i in range(times):
+                loader.loadData(loader.bin[i*256:(i+1)*256])
+                self.signalSetProgressbar.emit(i)
+                sleep(delay)
+
+            if remain is not 0:
+                i = i+1
+                loader.loadData(loader.bin[i*256:-1])
+                self.signalSetProgressbar.emit(i)
+                sleep(delay)
+
+            isOK = loader.lastData()
+            if isOK:
+                i = i+1
+                self.signalSetProgressbar.emit(i)
+            self.signalLastData.emit(isOK)
+        except serial.serialutil.SerialException as e:
+            self.signalGetSerialException.emit()
 
     def stop(self):
-        if self.shellIsRunning or self.p.poll() is None:
-            times = time.strftime("%H:%M:%S", time.gmtime())
-            self.signalGetLine.emit('[' + times + '] ' + 'Stop!' + '\n')
-            self.shellIsRunning = False
-            self.p.kill()
-        else:
-            pass
         self.terminate()
 
 # ---- class ShellThread End ---------------------------------------------------
@@ -47,34 +69,33 @@ class Asaprog(object):
     def __init__(self, widget, mainWindow):
         self.widget = widget
         self.mainWindow = mainWindow
+        self.widget.progressBar.setValue(0);
 
-        # ---- Shell Thread Init start -----------------------------------------
+        # ---- Thread Init start -----------------------------------------------
         self.shellThread = ShellThread()
-        self.shellThread.signalGetLine[str].connect(self.terminalInsertText)
-        # ---- Shell Thread Init end -------------------------------------------
+        self.shellThread.signalInitProgressbar[int,int].connect(self.initProgressbar)
+        self.shellThread.signalSetProgressbar[int].connect(self.setProgressbar)
+        self.shellThread.signalCheckIsAsaDevice[bool].connect(self.updateStatusIsAsaDevice)
+        self.shellThread.signalLastData[bool].connect(self.updateStatusLastData)
+        self.shellThread.signalGetSerialException.connect(self.getSerialException)
+        # ---- Thread Init end -------------------------------------------------
 
         # ---- Serial Group start ----------------------------------------------
         self.serial_updatePortlist()
         self.widget.pushButton_updatePortList.clicked.connect(self.serial_updatePortlist)
         # ---- Serial Group end ------------------------------------------------
 
-        # ---- Select File Group start -----------------------------------------
+        # ---- Basic Functions Group start -------------------------------------
         self.widget.pushButton_selectFile.clicked.connect(self.chooseProgFile)
-        # ---- Select File Group end -------------------------------------------
-
-        # ---- basic Tools Group start -----------------------------------------
+        self.widget.lineEdit_selectFile.textChanged.connect(self.checkIsHexFile)
         self.widget.pushButton_startProg.clicked.connect(self.startProg)
         self.widget.pushButton_stopProg.clicked.connect(self.stopProg)
-        # ---- basic Tools Group end -------------------------------------------
+        # ---- Basic Functions Group end ---------------------------------------
 
-        self.serial_updatePortlist()
-
-        # TODO: complete this two btn function
-        # self.widget.pushButton_settings.clicked.connect()
-        # self.widget.pushButton_progStk500.clicked.connect()
-
-    def terminalInsertText(self, s):
-        self.widget.textBrowser_terminal.insertPlainText(s)
+        # ---- Special Functions Group start -----------------------------------
+        # TODO: complete pushButton_progStk500 btn function
+        self.widget.pushButton_progStk500.clicked.connect(self.startProgStk500)
+        # ---- Special Functions Group end -------------------------------------
 
     # ---- Serial Group start --------------------------------------------------
     # Update port list in s_portComboBox
@@ -87,25 +108,94 @@ class Asaprog(object):
             self.widget.comboBox_selectPort.addItem(port)
     # ---- Serial Group end ----------------------------------------------------
 
-    # ---- Choose File Group start ---------------------------------------------
+    # ---- Basic Functions Group start -----------------------------------------
     def chooseProgFile(self):
-        name, _ = QFileDialog.getOpenFileName(self.mainWindow, 'Open File','', 'All Files (*);;Hex Files (*.hex)' ,initialFilter='Hex Files (*.hex)')
+        name, _ = QFileDialog.getOpenFileName(self.mainWindow,
+                                              'Open File',
+                                              '',
+                                              'All Files (*);;Hex Files (*.hex)',
+                                              initialFilter='Hex Files (*.hex)')
         if name is not '':
             self.widget.lineEdit_selectFile.setText(name)
-    # ---- Choose File Group end -----------------------------------------------
+            self.checkIsHexFile()
 
-    # ---- Basic Tools Group start ---------------------------------------------
+    def checkIsHexFile(self):
+        file = self.widget.lineEdit_selectFile.text()
+        try:
+            hexbin = py_asa_loader.parseHex(file)
+        except (NameError,UnicodeDecodeError):
+            self.widget.label_programSizeContent.setText(u"非HEX檔案格式，請重新選擇檔案")
+            self.widget.label_etcContent.setText('-')
+            return False
+        else:
+            size = len(hexbin)/1024
+            self.widget.label_programSizeContent.setText('{:0.2f} KB'.format(size))
+            etc = math.ceil(len(hexbin)/256)*0.03
+            self.widget.label_etcContent.setText('{:0.2f} s'.format(etc))
+            return True
+
     def startProg(self):
-        cmd = 'tools\\cmd_ASA_loader'
-        cmd += ' -p ' + self.widget.comboBox_selectPort.currentText().split('COM')[1]
-        cmd += ' -h ' + self.widget.lineEdit_selectFile.text()
-        self.shellThread.setCmd(cmd)
+        if self.shellThread.isRunning():
+            return
+        port = self.widget.comboBox_selectPort.currentText()
+        if port == '':
+            self.widget.label_statusContent.setText(u"未選擇串列埠")
+            return
+        hexfile = self.widget.lineEdit_selectFile.text()
+        if hexfile == '':
+            self.widget.label_statusContent.setText(u"未選擇燒錄檔案")
+            return
+        elif self.checkIsHexFile() is False:
+            self.widget.label_statusContent.setText(u"請重新選擇燒錄檔案")
+            return
+
+        self.widget.label_statusContent.setText(u"確認裝置中...")
+        self.shellThread.setParameter(port, hexfile)
         self.shellThread.start()
 
     def stopProg(self):
-        self.shellThread.stop()
-    # ---- Basic Tools Group end -----------------------------------------------
+        if self.shellThread.isRunning():
+            self.shellThread.stop()
+            self.widget.label_statusContent.setText(u"已強制終止")
+    # ---- Basic Functions Group end -------------------------------------------
 
-    # ---- Special Tools Group start -------------------------------------------
+    # ---- th Group start ------------------------------------------------------
+    def initProgressbar(self, min, max):
+        self.widget.progressBar.setRange(min, max)
 
-    # ---- Special Tools Group end ---------------------------------------------
+    def setProgressbar(self, val):
+        self.widget.progressBar.setValue(val)
+
+    def updateStatusIsAsaDevice(self, isAsaDevice):
+        if isAsaDevice:
+            self.widget.label_statusContent.setText(u"確認裝置為ASA_M128，開始燒錄")
+        else:
+            self.widget.label_statusContent.setText(u"請確認連接裝置為ASA_M128，並按下reset按鈕")
+
+    def updateStatusLastData(self ,isOK):
+        if isOK:
+            self.widget.label_statusContent.setText(u"燒錄成功！")
+        else:
+            self.widget.label_statusContent.setText(u"燒錄失敗！")
+
+    def getSerialException(self):
+        self.widget.label_statusContent.setText(u"與串列埠失去連線，請檢察USB線是否有連接好")
+
+    # ---- th Group end --------------------------------------------------------
+
+    # ---- Special Functions Group start ---------------------------------------
+    def startProgStk500(self):
+        # TODO: ADD STK500 hex file
+        pass
+        # if self.shellThread.isRunning():
+        #     return
+        # port = self.widget.comboBox_selectPort.currentText()
+        # if port == '':
+        #     self.widget.label_statusContent.setText(u"未選擇串列埠")
+        #     return
+        # hexfile = ''
+        #
+        # self.widget.label_statusContent.setText(u"確認裝置中...")
+        # self.shellThread.setParameter(port, hexfile)
+        # self.shellThread.start()
+    # ---- Special Functions Group end -----------------------------------------
