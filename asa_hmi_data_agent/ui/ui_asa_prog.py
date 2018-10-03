e8dcb8bad6bde9e39e0c5c06d63a14d4be4b8244
mickey9910326 <mickey9910326@gmail.com>
2018-10-03 14:10:11 +0800

    update setup.py and update avrdude version

--- asa_hmi_data_agent/ui/ui_asa_prog.py
+++ asa_hmi_data_agent/ui/ui_asa_prog.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ui/asa_prog.ui'
+# Form implementation generated from reading ui file 'ui\asa_prog.ui'
 #
 # Created by: PyQt5 UI code generator 5.9
 #
--- asa_hmi_data_agent/ui/ui_hmi.py
+++ asa_hmi_data_agent/ui/ui_hmi.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ui/hmi.ui'
+# Form implementation generated from reading ui file 'ui\hmi.ui'
 #
 # Created by: PyQt5 UI code generator 5.9
 #
