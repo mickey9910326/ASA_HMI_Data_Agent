cf170b1af0ed3b0f680568b3a188c3b7013f916f
mickey9910326 <mickey9910326@gmail.com>
2019-07-25 03:21:58 +0800

    完成基本下載功能及動態UI

--- asa_hmi_data_agent/adt_settings/adt_settings.py
+++ asa_hmi_data_agent/adt_settings/adt_settings.py
@@ -5,6 +5,44 @@ from asa_hmi_data_agent.listport import getAvailableSerialPorts
 from time import sleep
 import serial
 import math
+import requests
+
+
+class DownloadThread(QThread):
+    sigStart = pyqtSignal(int) # max
+    sigStepDone = pyqtSignal(int) #
+    sigAllDone = pyqtSignal()
+
+    def __init__(self):
+        QThread.__init__(self)
+
+    def run(self):
+        r = requests.get(
+            'https://github.com/mickey9910326/ASA_HMI_Data_Agent/releases/latest')
+        tag = r.url.split('/')[-1]
+        url = "https://github.com/mickey9910326/ASA_HMI_Data_Agent/releases/download/{}/ASA_HMI_Data_Agent_{}.zip".format(
+            tag, tag
+        )
+        filename = "ASA_HMI_Data_Agent_{}.zip".format(tag)
+        r = requests.get(url, stream=True)
+        total_size = int(r.headers.get('content-length', 0))
+        block_size = 2048
+        t = 0
+        wrote = 0
+        times = math.ceil(total_size//block_size)
+        self.sigStart.emit(times)
+                
+        with open(filename, 'wb') as f:
+            for data in r.iter_content(block_size):
+                wrote = wrote + len(data)
+                f.write(data)
+                t = t + 1
+                print("{}/{}".format(t, times))
+                self.sigStepDone.emit(t)
+
+        if total_size != 0 and wrote != total_size:
+            print("ERROR, something went wrong")
+
 
 class AdtSettings(QObject):
     widget = Ui_MainWidgetAdtSettings()
@@ -17,8 +55,19 @@ class AdtSettings(QObject):
         self.widget.setupUi(widget)
 
     def init(self):
+        self.dth = DownloadThread()
         self.widget.pushButton_termNumApply.clicked.connect(self.termNumApply)
+        # self.widget.pushButton_download.clicked.connect(self.downloadLastestFile)
+        self.widget.pushButton_download.clicked.connect(self.dth.start)
+        self.dth.sigStart[int].connect(self.download_start)
+        self.dth.sigStepDone[int].connect(
+            self.widget.progressBar_download.setValue)
 
     def termNumApply(self):
         num = int(self.widget.comboBox_termNum.currentText())
         self.signalTermNumApply.emit(num)
+
+    def download_start(self, times):
+        self.widget.progressBar_download.setValue(0)
+        self.widget.progressBar_download.setMaximum(times)
+        print('ok')
